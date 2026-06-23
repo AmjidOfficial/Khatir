01108e56e316a3409fef8845fa4ce678e8f07d99
Muhammad Amjid <amjid.bisconni@gmail.com>
2026-06-23 17:21:17 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/react-vite"; // or "@vitejs/plugin-react" depending on your exact package
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This ensures assets are referenced with the subfolder name prefix
-  base: "/Khatir/", 
+  base: "/Khatir/",
   plugins: [react()],
   resolve: {
     alias: {
